21a2954ec08bceb1dc31736597e8f97b483f98f9
Matt Way <matt__way@hotmail.com>
2019-04-10 09:56:40 +1000

    updated to be static version

--- code.js
+++ code.js
@@ -1,45 +1,30 @@
-
 export const run = ({ state, element, events, iteration }) => {
-  state.canvases = []
+  const { data, width, height } = state
+  const MAX_CANVASES = 1000
+  const totalCanvases = Math.min(data.length, MAX_CANVASES)
+
   element.style.padding = '2px 0px 0px 2px'
-}
 
-export const update = ({ state, element, events, iteration }) => {
-  const { data, canvases, width, height } = state
-  const totalCanvases = data.length
-  
-  if(totalCanvases < canvases.length){
-  	canvases.splice(totalCanvases) 
-  }
-  
-  for(var i=0; i<totalCanvases; i++){
-    if(i >= canvases.length){
-      const canvas = document.createElement('canvas')
-			canvases.push({
-        elem: canvas,
-        ctx: canvas.getContext('2d')
-      })
-      element.appendChild(canvas)
+  for (var c = 0; c < totalCanvases; c++) {
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+
+    canvas.width = width
+    canvas.height = height
+    canvas.style.display = 'inline-block'
+    canvas.style.margin = '0px 2px 2px 0'
+
+    element.appendChild(canvas)
+
+    const imageData = ctx.getImageData(0, 0, width, height)
+    const idata = imageData.data
+    const d = data[c]
+
+    for (var i = 0; i < idata.length; i += 4) {
+      const val = d[i / 4] * 255
+      idata[i] = idata[i + 1] = idata[i + 2] = val
+      idata[i + 3] = 255
     }
-    
-    const { elem, ctx } = canvases[i]
-    elem.width = width
-    elem.height = height
-    elem.style.display = 'inline-block'
-    elem.style.margin = '0px 2px 2px 0'
-  }  
-  
-  canvases.forEach(({ elem, ctx }, i) => {
-    const imageData = ctx.getImageData(0, 0, elem.width, elem.height)
-  	const idata = imageData.data 
-    const d = data[i]
-    
-    for(var i=0; i<idata.length; i+=4){
-    	const val = d[i/4] * 255
-      idata[i] = idata[i+1] = idata[i+2] = val    	
-      idata[i+3] = 255
-    }  
-    ctx.putImageData(imageData, 0, 0)   
-  })
+    ctx.putImageData(imageData, 0, 0)
+  }
 }
-
